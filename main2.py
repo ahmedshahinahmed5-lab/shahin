c7c4db22501116e662e4eea583b55664deb60408
ahmedshahinahmed5-lab <ahmedshahinahmed5@gmail.com>
2026-04-04 14:41:00 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -181,7 +181,6 @@ def classify_equation(eq_str):
 # STREAMLIT UI
 # =========================================================
 st.set_page_config(page_title="ODE Analyzer", page_icon="📘")
-st.image("el_ostora.webp", caption="Rizk Code", use_container_width=True)
 st.title("📘 Ordinary Differential Equation Solver — Streamlit Version")
 
 eq_input = st.text_input("Enter equation:", placeholder="Example: dy/dx + y = x*y**2")
